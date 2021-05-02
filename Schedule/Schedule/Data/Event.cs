cba282724190ccb7a820e560e4eb680f37d95006
chikibambony123 <67545135+chikibambony123@users.noreply.github.com>
2021-05-02 21:21:51 +0300

    Начал реализацию БД и функционала для кнопки добавления события

---
+++ Schedule/Schedule/Data/Event.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using SQLite;
+
+namespace Schedule.Data
+{
+    class Event
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public TimeSpan Time { get; set; }
+        public string Text { get; set; }
+
+        public Event(DateTime date, TimeSpan time, string text)
+        {
+            Date = date;
+            Time = time;
+            Text = text;
+        }
+    }
+}
--- Schedule/Schedule/Pages/CalendarPage.xaml.cs
+++ Schedule/Schedule/Pages/CalendarPage.xaml.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using SQLite;
 
 namespace Schedule.Pages
 {
@@ -15,6 +17,32 @@ namespace Schedule.Pages
         public CalendarPage()
         {
             InitializeComponent();
+            DateSelector.MinimumDate = DateTime.Now;
+            DateSelector.Date = DateTime.Now;
+            TimeSelector.Time = DateTime.Now.TimeOfDay.Add(new TimeSpan(0, 5, 0));
+        }
+
+        private void ButtonAddEvent_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(TextEditor.Text))
+            {
+                Device.BeginInvokeOnMainThread(async () =>
+                {
+                    await App.Current.MainPage.DisplayAlert("Ошибка", "Введите текст события", "OK");
+                });
+                return;
+            }
+
+            if (DateSelector.Date == DateTime.Now.Date && TimeSelector.Time < DateTime.Now.TimeOfDay.Add(new TimeSpan(0, 1, 0)))
+            {
+                Device.BeginInvokeOnMainThread(async () =>
+                {
+                    await App.Current.MainPage.DisplayAlert("Ошибка", "Минимальное установленное время должно быть через минуту", "OK");
+                });
+                return;
+            }
+
+            
         }
     }
 }
